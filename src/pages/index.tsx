0d90499849470852943da9380643516242b60ab6
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-04-17 23:06:29 +0330

    style: existing codes were formatted with prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { Roboto } from "next/font/google";
-import { twJoin } from "tailwind-merge";
+import { Roboto } from 'next/font/google';
+import { twJoin } from 'tailwind-merge';
 
-const roboto = Roboto({ weight: ["300", "400", "500"], subsets: ["latin"] });
+const roboto = Roboto({ weight: ['300', '400', '500'], subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
       className={twJoin(
         roboto.className,
-        "w-screen min-h-screen flex justify-center items-center"
+        'flex min-h-screen w-screen items-center justify-center'
       )}
     >
       Hello World
