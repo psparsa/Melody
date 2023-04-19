76b891d1cc085d43e9120ab9c32383f9040f3279
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-04-19 17:25:24 +0330

    chore: global/tailwind styles added to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
